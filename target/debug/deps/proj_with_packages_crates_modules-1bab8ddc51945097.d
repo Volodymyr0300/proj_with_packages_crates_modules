f8d6de8541efaade45559673522582c311f6b074
Volodymyr Poliakov <vladimir03002@gmail.com>
2025-05-31 09:52:18 -0500

    isitial_commit

---
+++ target/debug/deps/proj_with_packages_crates_modules-1bab8ddc51945097.d
@@ -0,0 +1,5 @@
+/Users/admin/Documents/rust/proj_with_packages_crates_modules/target/debug/deps/libproj_with_packages_crates_modules-1bab8ddc51945097.rmeta: src/main.rs
+
+/Users/admin/Documents/rust/proj_with_packages_crates_modules/target/debug/deps/proj_with_packages_crates_modules-1bab8ddc51945097.d: src/main.rs
+
+src/main.rs:
---
+++ target/debug/deps/proj_with_packages_crates_modules-7d080b8afe5f7f25.d
@@ -0,0 +1,5 @@
+/Users/admin/Documents/rust/proj_with_packages_crates_modules/target/debug/deps/libproj_with_packages_crates_modules-7d080b8afe5f7f25.rmeta: src/main.rs
+
+/Users/admin/Documents/rust/proj_with_packages_crates_modules/target/debug/deps/proj_with_packages_crates_modules-7d080b8afe5f7f25.d: src/main.rs
+
+src/main.rs:
